e7a3dd68c6f5dbc1e7c316c5e2823e9f9ef21371
billwright <bill@wwwright.com>
2023-02-26 12:00:01 -0600

    First test passing with hard-coded first name

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,23 @@
+import { Appointment } from "../src/Appointment";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { act } from "react-dom/test-utils";
+
+
 describe("Appointment", () => {
-	it("renders the customer's first name", () => {
-	});
+    it("renders the customer's first name", () => {
+        const customer = { firstName: "Ashley" };
+        const component = (
+            <Appointment customer={customer} />
+        );
+        const container = document.createElement("div");
+        document.body.appendChild(container);
+
+        act(() =>
+	    ReactDOM.createRoot(container).render(component)
+        );
+
+        expect(document.body.textContent).toContain("Ashley");
+    });
 });
 
